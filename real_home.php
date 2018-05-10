7439507517e8f43e9775e35b590eac5df3887379
joker <joker_xii@outlook.com>
2018-05-11 01:23:09 +0800

    changing domain

--- real_home.php
+++ real_home.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="utf-8"/>
     <title>MUSIC</title>
-    <link href="https://resources.joker.services/Content/bootstrap.css" rel="stylesheet"/>
+    <link href="https://res.joker.im/Content/bootstrap.css" rel="stylesheet"/>
     <link href="styles.css" media="all" rel="stylesheet"/>
     <link href="scrollbar.css" media="all" rel="stylesheet"/>
-    <link rel="icon" href="https://resources.joker.services/favicon_music.ico" type="image/x-icon"/>
-    <link rel="shortcut icon" href="https://resources.joker.services/favicon_music.ico" type="image/x-icon"/>
-    <link href="https://resources.joker.services/Fonts/css/fontawesome.css" media="all" rel="stylesheet"/>
-    <link href="https://resources.joker.services/Fonts/css/fontawesome-all.css" media="all" rel="stylesheet"/>
+    <link rel="icon" href="https://res.joker.im/favicon_music.ico" type="image/x-icon"/>
+    <link rel="shortcut icon" href="https://res.joker.im/favicon_music.ico" type="image/x-icon"/>
+    <link href="https://res.joker.im/Fonts/css/fontawesome.css" media="all" rel="stylesheet"/>
+    <link href="https://res.joker.im/Fonts/css/fontawesome-all.css" media="all" rel="stylesheet"/>
     <meta name="description" content="Music Box by Joker"/>
     <script type="text/javascript">
         var song_id_now;
@@ -202,8 +202,8 @@
         </div>
     </div>
 </div>
-<script src="https://resources.joker.services/Scripts/bootstrap.js"></script>
-<script type="text/javascript" src="https://resources.joker.services/Scripts/jquery-3.1.1.js"></script>
+<script src="https://res.joker.im/Scripts/bootstrap.js"></script>
+<script type="text/javascript" src="https://res.joker.im/Scripts/jquery-3.1.1.js"></script>
 <script type="text/javascript" src="switchbg.js"></script>
 <script src="utilities.js" type="text/javascript"></script>
 <!--<script src="firestar2.js"></script>-->
